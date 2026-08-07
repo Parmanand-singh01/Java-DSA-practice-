9975c8bd8b28d82f308ae28eed0a2ee6b748c70f
Parmanand-singh01 <singhparmanand921@gmail.com>
2026-08-08 01:31:50 +0530

    Add LargestAltitude class to calculate max altitude

---
+++ Maths/Largest altitude.java
@@ -0,0 +1,9 @@
+class Solution {
+    public int largestAltitude(int[] gain) {
+        int max=0,sum=0;
+for(int i=0;i<gain.length;i++){
+    max=Math.max(max,sum+=gain[i]);
+}
+        return max;
+    }
+}
