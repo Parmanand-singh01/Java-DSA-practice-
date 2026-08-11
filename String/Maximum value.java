c63a422851f50cb5f83d3181560b61f591adee3b
Parmanand-singh01 <singhparmanand921@gmail.com>
2026-08-11 22:35:25 +0530

    Implement maximumValue method to find max length or value

---
+++ String/Maximum value.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int maximumValue(String[] strs) {
+        int max=0;
+        for(String s:strs){
+            if(s.matches("\\d+")){
+max=Math.max(max,Integer.parseInt(s));
+            } else {
+max=Math.max(max,s.length());
+            }
+        }
+        return max;
+    }
+}
