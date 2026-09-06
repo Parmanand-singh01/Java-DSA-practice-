ccc3eef78934c2f3a286fb02801c9bd6789d93aa
Parmanand-singh01 <singhparmanand921@gmail.com>
2026-09-06 16:04:50 +0530

    Add numOfPairs method to count valid pairs

---
+++ Maths/NoOfPair.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int numOfPairs(String[] nums, String target) {
+        int count=0;
+        int n=nums.length;
+        for(int i=0;i<n;i++){
+            for(int j=0;j<n;j++){
+                if((nums[i]+nums[j]).equals(target) && i!=j){
+                    count++;
+                }
+            }
+        }
+        return count;
+        
+    }
+}
