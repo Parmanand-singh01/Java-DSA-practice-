1286bb8d1f63887cd7bb029549b848328958473a
Parmanand-singh01 <singhparmanand921@gmail.com>
2026-09-12 18:53:12 +0530

    Implement method to find repeated element in array

---
+++ Hashmap,Set/ReapeatedNTime.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int repeatedNTimes(int[] nums) {
+        HashSet<Integer> set =new HashSet<>();
+        for(int i:nums){
+            if(set.contains(i)) return i;
+            else 
+            set.add(i);
+        }
+        return 0;
+        
+    }
+}
